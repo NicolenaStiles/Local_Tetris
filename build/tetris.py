1ebf7d647bf20e8024242565ae4504b881972650
NicolenaStiles <nicolena.stiles@gmail.com>
2020-06-14 00:35:42 -0400

    Adding main entry point.

---
+++ build/tetris.py
@@ -0,0 +1,25 @@
+# Main file for Pygame based Tetris
+#
+#
+# Created by Nicolena Stiles
+# 06/14/2020
+
+################
+# IMPORTS
+################
+
+# For interfacting with system.
+import sys
+
+# For game-related support using pygame
+import pygame
+
+################
+# MAIN FUNCTION
+################
+
+def main():
+    print("Hello and welcome to Tetris!")
+
+if __name__ == "__main__":
+    main()
